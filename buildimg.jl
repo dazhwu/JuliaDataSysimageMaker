b736098b0745d45e1df7b7c5d2b653bb2a9dece4
dazhwu <63244649+dazhwu@users.noreply.github.com>
2022-12-20 22:29:36 -0500

    Update buildimg.jl

--- buildimg.jl
+++ buildimg.jl
@@ -4,7 +4,7 @@ Pkg.instantiate()
 using PackageCompiler
 using Pkg
 
-Pkg.add(["StatsPlots","CSV","DataFrames","DataFramesMeta","SQLite","GLM","Optim","Distributions","Turing","MCMCChains","Zygote","ReverseDiff","Memoization","Revise","StatsBase","StatsFuns"])
+Pkg.add(["StatsPlots","CSV","DataFrames","DataFramesMeta","SQLite","GLM","Optim","Distributions","Turing","MCMCChains","Zygote","ReverseDiff","Memoization","Revise","StatsBase","StatsFuns","ODBC","Plots"])
 
-create_sysimage([:StatsPlots,:CSV,:DataFrames,:DataFramesMeta,:SQLite,:GLM,:Optim,:Distributions,:Turing,:MCMCChains,:Zygote,:ReverseDiff,:Memoization,:Revise],sysimage_path="/var/local/dlakelan/julia/sys_dataanalys.so",precompile_execution_file="dataanalys.jl")
+create_sysimage([:StatsPlots,:CSV,:DataFrames,:DataFramesMeta,:SQLite,:GLM,:Optim,:Distributions,:Turing,:MCMCChains,:Zygote,:ReverseDiff,:Memoization,:Revise,:StatsBase, :StatsFuns, :ODBC,:Plots],sysimage_path="/var/local/dlakelan/julia/sys_dataanalys.so",precompile_execution_file="dataanalys.jl")
 
