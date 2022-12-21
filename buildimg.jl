fdd80cf25764a7d0f63a48135470c5c9db3c9c28
dazhwu <63244649+dazhwu@users.noreply.github.com>
2022-12-20 22:43:05 -0500

    Update buildimg.jl

--- buildimg.jl
+++ buildimg.jl
@@ -7,5 +7,5 @@ using Pkg
 Pkg.add(["StatsPlots","CSV","DataFrames","DataFramesMeta","SQLite","GLM","Optim","Distributions","Turing","MCMCChains","Zygote","ReverseDiff","Memoization","Revise","StatsBase","StatsFuns"])
 Pkg.add(["ODBC","Plots","MKL","RCall"])
 
-create_sysimage([:StatsPlots,:CSV,:DataFrames,:DataFramesMeta,:SQLite,:GLM,:Optim,:Distributions,:Turing,:MCMCChains,:Zygote,:ReverseDiff,:Memoization,:Revise,:ODBC,:Plots,:MKL, :RCall],sysimage_path="/var/local/dlakelan/julia/sys_dataanalys.so",precompile_execution_file="dataanalys.jl")
+create_sysimage([:StatsPlots,:CSV,:DataFrames,:DataFramesMeta,:SQLite,:GLM,:Optim,:Distributions,:Turing,:MCMCChains,:Zygote,:ReverseDiff,:Memoization,:Revise,:ODBC,:Plots,:MKL, :RCall],sysimage_path="sys.so",precompile_execution_file="dataanalys.jl")
 
